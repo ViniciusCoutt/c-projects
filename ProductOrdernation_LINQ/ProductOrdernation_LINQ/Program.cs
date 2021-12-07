6a0a383d235563e0bdc2d3d630a850c98c8c8032
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-12-07 19:58:32 -0300

    LINQ Exercises

---
+++ ProductOrdernation_LINQ/ProductOrdernation_LINQ/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ProductOrdernation_LINQ.Entities;
+using System.Globalization;
+using System.Linq;
+
+static void Print<T>(IEnumerable<T> collection)
+{
+    foreach (T obj in collection)
+    {
+        Console.WriteLine(obj);
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter full file path: ");
+string path = Console.ReadLine();
+
+using (StreamReader sr = File.OpenText(path))
+{
+    List<Product> products = new List<Product>();
+
+    while (!(sr.EndOfStream))
+    {
+        string[] line = sr.ReadLine().Split(',');
+        string name = line[0];
+        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+        products.Add(new Product(name, price));
+    }
+
+    var avarage = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+
+    Console.WriteLine("Avarage price: " + avarage.ToString("F2"));
+
+    var result = products.Where(p => p.Price < avarage).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+    Print(result);
+
+}
+
+
+
